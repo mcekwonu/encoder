07a7cd5af390caebd2b1572237e43500bbd064bf
mcekwonu <cekwonu@gmail.com>
2021-04-13 19:17:21 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+
+__author__ = 'Ekwonu Michael Chukwuemeka'
+
+__version__ = '0.0.1'
+
+__email__ = 'deepstarai@gmail.com'
+
+__date__ = '2021.04.13'
+
+__affiliation__ = 'Experimental Thermo-Fluids, Mechanics and Energy Systems Lab '
---
+++ one_hot_encoder.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class OneHotEncoder:
+	"""
+	One hot encoder class and encodes categorical labels
+	"""
+	def __init__(self, label):
+		self.label = label
+		self.n_classes = len(np.unique(self.label))
+	
+	@property
+	def transform(self):
+		"""
+		Fits and transforms the input labels into one hot encoding
+		"""
+		if isinstance(self.label, list):
+			y = self.label
+		else:
+			y = self.label.flatten()
+		n_classes = len(np.unique(y))
+		labels = dict(zip(np.unique(y), np.arange(n_classes)))
+		targets = np.vectorize(labels.get)(y)
+		return np.eye(n_classes)[targets]
+		
+	@property
+	def inverse_transform(self):
+		"""
+		Returns the input labels
+		"""
+		return np.array(self.label)
+	
+	@property
+	def classes_(self):
+		"""
+		Sorts and returns the class labels
+		"""
+		return np.unique(self.label)
+		
+	# Alias
+	fit_transform = transform
